5e9829e58eb813225582dad7b24a553a3f669245
John DeSilva <desilvjo@umich.edu>
2021-04-22 10:26:36 -0400

    Update rename script for bin directory

--- rename.sh
+++ rename.sh
@@ -10,3 +10,10 @@ fi
 find ./lib -type f -exec sed -i'' -e 's/Approvals/Goldeen/g' {} +
 find ./lib -type f -exec sed -i'' -e 's/approvals\//goldeen\//g' {} +
 find ./lib -type f -exec sed -i'' -e "s/require 'approvals'/require 'goldeen'/g" {} +
+
+if [ -f bin/approvals ]; then
+  mv bin/approvals bin/goldeen
+fi
+
+find ./bin -type f -exec sed -i'' -e 's/Approvals/Goldeen/g' {} +
+find ./bin -type f -exec sed -i'' -e "s/require 'approvals/require 'goldeen/g" {} +
